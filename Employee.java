01fa3857e850dfe9dc50c7d1e7dd55022ef76dca
shubhamkamble <kshub44kamble@gmail.com>
2020-04-30 19:20:13 +0530

    ADDED: Part Time Employee Wage

--- Employee.java
+++ Employee.java
@@ -3,28 +3,37 @@
   *@author: shubhamkamble
 */
 import java.util.Random;
-
-public class Employee {
-	//Constant.
-	public static final int IS_EMP_PRESENT = 1;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	/**
-	  *Class Calculating Daily Wage Of Employee.
+public class Employee
+{
+    //Constants
+    public static final int IS_EMP_FULL_TIME=1;
+    public static final int IS_EMP_PART_TIME=2;
+    public static final int EMP_RATE_PER_HOUR=20;
+    /**
+	   *Calculating daily wage of employee
 	*/
-	public static void calculateDailyWage() {
-		//Variables
-		int empHours = 0;
-		int empDailyWage = 0;
+    public static void calculateDailyWage()
+    {
+        //Variables
+        int empHours=0;
+        int empDailyWage=0;
 
-		Random randNo = new Random();
-		int empCheck = randNo.nextInt()%2;
-		if(empCheck == IS_EMP_PRESENT) {
-			empHours = 8;
-		}
-		empDailyWage = empHours * EMP_RATE_PER_HOUR;
-		System.out.println("Employee Daily Wage="+empDailyWage);
-	}
-	public static void main(String[] args) {
-	calculateDailyWage();
-	}
+        Random randNo=new Random();
+        int empCheck=randNo.nextInt()%3;
+        if(empCheck==IS_EMP_FULL_TIME)
+        {
+            empHours=8;
+        }
+        else if(empCheck==IS_EMP_PART_TIME)
+        {
+            empHours=4;
+        }
+        empDailyWage=empHours*EMP_RATE_PER_HOUR;
+        System.out.println("Employee Daily wage="+empDailyWage);
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to employee wage computation");
+        calculateDailyWage();
+    }
 }
