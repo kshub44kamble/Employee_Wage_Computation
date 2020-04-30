275bd074668e63b49d9c24a5cdecdb143441ba4d
shubhamkamble <kshub44kamble@gmail.com>
2020-04-30 21:31:59 +0530

    ADDED: Calculate Wages for a month

--- Employee.java
+++ Employee.java
@@ -9,34 +9,40 @@ public class Employee
     public static final int IS_EMP_FULL_TIME=1;
     public static final int IS_EMP_PART_TIME=2;
     public static final int EMP_RATE_PER_HOUR=20;
+    public static final int WORKING_DAYS_IN_MONTH=20;
     /**
-	   *Calculating daily wage of employee
+	   *Calculating monthly wage of employee
 	*/
-    public static void calculateDailyWage()
+    public static void calculateMonthlyWage()
     {
         //Variables
         int empHours=0;
         int empDailyWage=0;
+        int empMonthlyWage=0;
 
-        Random randNo=new Random();
-        int empCheck=randNo.nextInt()%3;
-        switch (empCheck)
+        for(int i=1;i<=WORKING_DAYS_IN_MONTH;i++)
         {
-            case IS_EMP_FULL_TIME:
-                            empHours=8;
+            Random randNo = new Random();
+            int empCheck = randNo.nextInt() % 3;
+            switch (empCheck)
+            {
+                case IS_EMP_FULL_TIME:
+                            empHours = 8;
                             break;
-            case IS_EMP_PART_TIME:
-                            empHours=4;
+                case IS_EMP_PART_TIME:
+                            empHours = 4;
                             break;
-            default:
-                empHours=0;
+                default:
+                            empHours = 0;
+            }
+            empDailyWage = empHours * EMP_RATE_PER_HOUR;
+            empMonthlyWage=empMonthlyWage+empDailyWage;
         }
-        empDailyWage=empHours*EMP_RATE_PER_HOUR;
-        System.out.println("Employee Daily wage="+empDailyWage);
+        System.out.println("Employee monthly wage="+empMonthlyWage);
     }
     public static void main(String[] args)
     {
         System.out.println("Welcome to employee wage computation");
-        calculateDailyWage();
+        calculateMonthlyWage();
     }
 }
